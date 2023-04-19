0effd6fce772349608c0fb6baa8230bf16fe90e7
WalterIruegas <walter.iruegas@outlook.com>
2023-04-18 20:10:07 -0500

    navbar update button sesion

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
-import { Navbar, Container, Button } from 'react-bootstrap';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
 
 const AppNavbar = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setLoggedIn(true);
+  }
+
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand>Filmoteca</Navbar.Brand>
-        <Button variant="warning">Iniciar Sesión</Button>
+        <Navbar.Brand>FILMOTECA</Navbar.Brand>
+        {loggedIn ? (
+          <Nav className="me-auto">
+            <Nav.Link href="#rollos">Rollos</Nav.Link>
+            <Nav.Link href="#publicacion">Crear publicación</Nav.Link>
+            <Nav.Link href="#perfil">Perfil</Nav.Link>
+            <Nav.Link href="#about">Sobre nosotros</Nav.Link>
+          </Nav>
+        ) : (
+          <Button variant="warning" onClick={handleLogin}>Iniciar Sesión</Button>
+        )}
       </Container>
     </Navbar>
   );
