e351b07718ec242c2ac01c11d366dd3f2b4c02f4
Fernando <fer_mdogtz2222@icloud.com>
2023-04-19 00:29:34 -0600

    Componente rollos populares

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,36 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { getSaludo } from '../api/apiSaludo'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getSaludo } from '../api/apiSaludo';
+import RollosPopulares from '../components/RollosPopulares/RollosPopulares';
 
 const Home = () => {
-  const [miSaludo, setMiSaludo] = useState('');
+	const [miSaludo, setMiSaludo] = useState('');
 
-  const handleSaludo = async()=>{
-    console.log('Perdiremos saludo:..');
-    try {
-      const response = await getSaludo();
-      console.log('response:..',response);
-      if(response?.data){
-        setMiSaludo(response.data);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
+	const handleSaludo = async () => {
+		console.log('Perdiremos saludo:..');
+		try {
+			const response = await getSaludo();
+			console.log('response:..', response);
+			if (response?.data) {
+				setMiSaludo(response.data);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  return (
-    <div>Home
-        <Link to={'/about'}>ABOUT</Link>
-        <br/>
-        <button onClick={handleSaludo} className="btn btn-primary">GetSaludo</button>
-        {miSaludo&& <h5>{miSaludo}</h5> }
-    </div>
-  )
-}
+	return (
+		<div>
+			Home
+			<Link to={'/about'}>ABOUT</Link>
+			<RollosPopulares />
+			<br />
+			<button onClick={handleSaludo} className="btn btn-primary">
+				GetSaludo
+			</button>
+			{miSaludo && <h5>{miSaludo}</h5>}
+		</div>
+	);
+};
 
-export default Home
+export default Home;
