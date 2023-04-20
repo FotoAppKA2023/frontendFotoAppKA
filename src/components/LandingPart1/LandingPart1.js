dbad4a711b100fa0ed2f9ae9f194b71fbeed23db
ror2022 <fotoappkodemia@gmail.com>
2023-04-20 12:22:55 -0600

    integrando landingParts

--- src/components/LandingPart1/LandingPart1.js
+++ src/components/LandingPart1/LandingPart1.js
@@ -1,27 +1,11 @@
 import React from "react";
 import "./LandingPart1.css";
-<<<<<<< HEAD
-=======
 import { Container, Row, Col } from 'react-bootstrap';
->>>>>>> landingPart2
 
 const LandingPart1 = () => {
   return (
     <Container fluid className="landing-page">
       <div className="overlay"></div>
-<<<<<<< HEAD
-      <div className="landing-header">
-        <h1>Conviértete en experto capturando momentos que sobresalen</h1>
-      </div>
-      <div className="landing-footer">
-        <p className="left-align">
-          La película de cine encarretada para foto fija otorga una nueva arista
-          al mundo análogo, desde los tonos, la versatilidad y su funcionalidad.
-        </p>
-        <p className="right-align">Kodak vision &rarr;</p>
-      </div>
-    </div>
-=======
       <Row>
         <Col>
           <div className="landing-header">
@@ -41,7 +25,6 @@ const LandingPart1 = () => {
         </Col>
       </Row>
     </Container>
->>>>>>> landingPart2
   );
 };
 
