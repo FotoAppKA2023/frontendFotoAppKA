c38ae282a0a5ffc0b38e36fe0032bb108527e04a
ror2022 <fotoappkodemia@gmail.com>
2023-04-20 12:10:21 -0600

    integrando landingPart1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar.js";
 import RollosPopulares from "../components/RollosPopulares/RollosPopulares.js";
-import Navbar from "../components/Navbar/Navbar";
 import LandingPart1 from "../components/LandingPart1/LandingPart1";
 
 const Home = () => {
