0c74387feb11b9a3d2d17292909258ba31895632
WalterIruegas <walter.iruegas@outlook.com>
2023-04-20 00:22:36 -0500

    new branch landingPart2 updatev3

--- src/components/LandingPart1/LandingPart1.js
+++ src/components/LandingPart1/LandingPart1.js
@@ -1,21 +1,30 @@
 import React from "react";
 import "./LandingPart1.css";
+import { Container, Row, Col } from 'react-bootstrap';
 
 const LandingPart1 = () => {
   return (
-    <div className="landing-page">
+    <Container fluid className="landing-page">
       <div className="overlay"></div>
-      <div className="landing-header">
-        <h1>Conviértete en experto capturando momentos que sobresalen</h1>
-      </div>
-      <div className="landing-footer">
-        <p className="left-align">
-          La película de cine encarretada para foto fija otorga una nueva arista
-          al mundo análogo, desde los tonos, la versatilidad y su funcionalidad.
-        </p>
-        <p className="right-align">Kodak vision &rarr;</p>
-      </div>
-    </div>
+      <Row>
+        <Col>
+          <div className="landing-header">
+            <h1>Conviértete en experto capturando momentos que sobresalen</h1>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div className="landing-footer">
+            <p className="left-align">
+              La película de cine encarretada para foto fija otorga una nueva arista
+              al mundo análogo, desde los tonos, la versatilidad y su funcionalidad.
+            </p>
+            <p className="right-align">Kodak vision &rarr;</p>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
