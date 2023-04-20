dd1a28d0d74525fbb39cdb1965c461a7bdc2e3d1
WalterIruegas <walter.iruegas@outlook.com>
2023-04-19 23:49:46 -0500

    new branch landingPart2 updatev2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import LandingPart1 from '../components/LandingPart1/LandingPart1';
+import LandingPart2 from '../components/LandingPart2/LandingPart2';
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <LandingPart1/>
+      <LandingPart2/>
     </div>
   );
 }
