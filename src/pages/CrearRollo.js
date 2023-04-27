a5c517eaa28c233b074f7c7622c87d020f9f93a5
MiguelIbanezKodemia <miguepumas2010@gmail.com>
2023-04-26 23:10:49 -0600

    formulario de creacion de rollos v2-funcionaildad completa

--- src/pages/CrearRollo.js
+++ src/pages/CrearRollo.js
@@ -1,100 +1,144 @@
 import React from "react";
+import { ReactDOM } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import imagen from "../assets/imagen-placeholder.png";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import axios from 'axios';
+
 const CrearRollo = () => {
-    
-      
-    
-   const  abrirInputFile=()=>{
-        console.log("se debe ejecutar la abrida de archivos")
-    }
+  const [datos,setDatos]= React.useState({
+    formato:'',
+    marca:'',
+    nombre:'',
+    iso:'',
+    grano:'',
+    tomas:'',
+    descripcion:'',
+
+
+  })
+  const [foto,setFoto]=React.useState({
+    file:File
+  })
+
+  const imprimirDatos=()=>{
+    console.log(datos)
+    const formData=new FormData()
+    formData.append('imagenRollo',foto)
+    formData.append("datosRollos",datos)
+    axios.post("url",formData,{}).then(res=>{
+      console.log(res)
+    })
+
+  }
+  const abrirInputFile = () => {
+    console.log("se debe ejecutar la abrida de archivos");
+  };
+  const obtenerFile=(event)=>{
+     console.log(event.target)
+    setFoto({
+      foto,
+      [event.target.name]:event.target.files[0]
+    })
+  }
+  const obtenerValor=(event)=>{
+    setDatos({
+      ...datos,
+      [event.target.name]:event.target.value
+    })
+  }
   return (
-    <>
-      <Container>
-   <div>
-   <img
-          src={imagen}
-          style={{ width: "100px", marginLeft: "45%" }}
-          alt=""
-          onClick={abrirInputFile()}
-        />
-        <Form.Group  controlId="formFile" className="mb-3">
-          <Form.Control type="file" />
-        </Form.Group>
-   </div>
-
-        <Form>
-          <Form.Group className="mb-3">
-            <Form.Label>Formato</Form.Label>
-            <Form.Select>
-              <option>10</option>
-              <option>20</option>
-              <option>30</option>
-              <option>40</option>
-            </Form.Select>
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Marca</Form.Label>
-            <Form.Select>
-              <option>10</option>
-              <option>20</option>
-              <option>30</option>
-              <option>40</option>
-            </Form.Select>
-          </Form.Group>
-
-          <Form.Group className="mb-3">
-            <Form.Label>Nombre</Form.Label>
-            <Form.Select>
-              <option>10</option>
-              <option>20</option>
-              <option>30</option>
-              <option>40</option>
-            </Form.Select>
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>ISO</Form.Label>
-            <Form.Select>
-              <option>10</option>
-              <option>20</option>
-              <option>30</option>
-              <option>40</option>
-            </Form.Select>
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Grano</Form.Label>
-            <Form.Select>
-              <option>10</option>
-              <option>20</option>
-              <option>30</option>
-              <option>40</option>
-            </Form.Select>
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Numero de tomas</Form.Label>
-            <Form.Select>
-              <option>10</option>
-              <option>20</option>
-              <option>30</option>
-              <option>40</option>
-            </Form.Select>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicDescripcion">
-            <Form.Label>Descripcion</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Ingresa la descripcion del rollo"
+    <Container>
+      <Row >
+        <Col ></Col>
+        <Col >
+          <div>
+            <img
+              src={imagen}
+              style={{ width: "100px", marginLeft: "45%" }}
+              alt=""
+              onClick={abrirInputFile}
             />
-          </Form.Group>
+            <Form.Group controlId="formFile" className="mb-3">
+              <Form.Control name="file" onChange={obtenerFile} type="file" />
+            </Form.Group>
+          </div>
 
-          <Button variant="primary" type="submit">
-            Agregar Rollo
-          </Button>
-        </Form>
-      </Container>
-    </>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Formato</Form.Label>
+              <Form.Select name="formato" onChange={obtenerValor}>
+                <option>10</option>
+                <option>20</option>
+                <option>30</option>
+                <option>40</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Marca</Form.Label>
+              <Form.Select name="marca" onChange={obtenerValor}>
+                <option>10</option>
+                <option>20</option>
+                <option>30</option>
+                <option>40</option>
+              </Form.Select>
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Nombre</Form.Label>
+              <Form.Select name="nombre" onChange={obtenerValor}>
+                <option>10</option>
+                <option>20</option>
+                <option>30</option>
+                <option>40</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>ISO</Form.Label>
+              <Form.Select name="iso" onChange={obtenerValor}>
+                <option>10</option>
+                <option>20</option>
+                <option>30</option>
+                <option>40</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Grano</Form.Label>
+              <Form.Select name="grano" onChange={obtenerValor}>
+                <option>10</option>
+                <option>20</option>
+                <option>30</option>
+                <option>40</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Numero de tomas</Form.Label>
+              <Form.Select name="tomas" onChange={obtenerValor}>
+                <option>10</option>
+                <option>20</option>
+                <option>30</option>
+                <option>40</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicDescripcion">
+              <Form.Label>Descripcion</Form.Label>
+              <Form.Control
+                name="descripcion"
+                onChange={obtenerValor}
+                type="text"
+                placeholder="Ingresa la descripcion del rollo"
+              />
+            </Form.Group>
+
+            <Button onClick={imprimirDatos} >Agregar Rollo</Button>
+          </Form>
+        </Col>
+        <Col ></Col>
+      </Row>
+    </Container>
   );
 };
 
