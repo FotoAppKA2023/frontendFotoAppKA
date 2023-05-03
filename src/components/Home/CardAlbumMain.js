7feb4fa16eefbc26aad715096cdf0519966be69f
MiguelIbanezKodemia <miguepumas2010@gmail.com>
2023-05-03 00:28:37 -0600

    crear dashboard sin peticiones

---
+++ src/components/Home/CardAlbumMain.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Nav from "react-bootstrap/Nav";
+export default function CardAlbumMain({ datos }) {
+  return (
+    <Card style={{ width: "30%" }}>
+      <Card.Img variant="top" src={datos.imagenUrl} />
+
+      <Card.Body>
+        <hr />
+        <Card.Text>
+          <Nav
+            activeKey="/home"
+            onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+          >
+            <Nav.Item>
+              <Nav.Link href="/rollos/:id">Rollo</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-1">Scanner</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-2">Camara</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,16 +1,83 @@
+import CardAlbumMain from "../components/Home/CardAlbumMain";
+import CardAlbumAside from "../components/Home/CardAlbumASide";
+export default function Dashboard() {
+  const albumModel = {
+    imagenUrl:
+      "https://1819.es/wp-content/uploads/2020/04/El-coleccionismo-de-fotograf%C3%ADas-antiguas.jpg",
+    idRollo: "2",
+    idScanner: "3",
+    idCamara: "4",
+  };
+  const rolloModel = {
+    imagenUrl:
+      "https://www.fotojapon.com/media/images/products/641/16787441857wHYk-350.jpg",
+    idRollo: "2",
+    idScanner: "3",
+    idCamara: "4",
+  };
 
+  return (
+    <div
+      style={{
+        display: "flex",
+    
 
-
-export default function Dashboard(){
-
-    return(
-        <>
-            <div>
-                <aside style={{width:"15%", height:"650px", border:"1px solid black"}}>
-                    <h1>hjdckjbdfjkdfjk</h1>
-                </aside>
-            </div>
-        </>
-    )
+        width: "100%",
+      }}
+    >
+      <aside
+        style={{
+          width: "25%",
+          height: "100vh",
+        
+          display: "flex",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            height: "100%",
+            flexDirection: "column",
+            overflowY: "scroll",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <h2>Albumes favoritos</h2>
+          <CardAlbumAside datos={albumModel} />
+          <CardAlbumAside datos={albumModel} />
+          <CardAlbumAside datos={albumModel} />
+          <CardAlbumAside datos={albumModel} />
+          <h2>Rollos Favoritos</h2>
+          <CardAlbumAside datos={rolloModel} />
+          <CardAlbumAside datos={rolloModel} />
+          <CardAlbumAside datos={rolloModel} />
+          <CardAlbumAside datos={rolloModel} />
+        </div>
+      </aside>
+      <main
+        style={{
+         
+          width: "70%",
+          display: "flex",
+          gap: "5%",
+          flexWrap: "wrap",
+        }}
+      >
+        <CardAlbumMain datos={albumModel} />
+        <CardAlbumMain datos={albumModel} />
+        <CardAlbumMain datos={albumModel} />
+        <CardAlbumMain datos={albumModel} />
+        <CardAlbumMain datos={albumModel} />
+        <CardAlbumMain datos={albumModel} />
+        <CardAlbumMain datos={albumModel} />
+        <CardAlbumMain datos={albumModel} />
+        <CardAlbumMain datos={albumModel} />
+        <CardAlbumMain datos={albumModel} />
+        <CardAlbumMain datos={albumModel} />
+        <CardAlbumMain datos={albumModel} />
+      </main>
+    </div>
+  );
 }
-
