ccfc0ab39d63e73c77ba70ef56c1cf735d19725c
Fernando <fer_mdogtz2222@icloud.com>
2023-05-09 22:51:50 -0600

    componente perfil

--- src/components/PerfilUsuario/PerfilUsuario.js
+++ src/components/PerfilUsuario/PerfilUsuario.js
@@ -1,30 +1,66 @@
-import * as React from "react";
-import Foto from "../../assets/imgRollosPopulares/Rollo1.png";
-import Image from "react-bootstrap/Image";
-
+import * as React from 'react';
+import Foto from '../../assets/imgRollosPopulares/Rollo1.png';
+import wallpaper from '../../assets/imgRollosPopulares/wallpaper.png';
+import Image from 'react-bootstrap/Image';
+import { Link } from 'react-router-dom';
+import './PerfilUsuario.css';
+import usePhoto from '../../hooks/usePhoto';
+import { useEffect } from 'react';
+import { getMyAlbumsByIdPhotoUser } from '../../api/apiAlbums';
+import { useState } from 'react';
+import CardAlbumMain from '../Home/CardAlbumMain';
+import { myId } from '../../lib/myLib';
 const PerfilUsuario = () => {
-  return (
-    <>
-      <section>
-        <div className="userPhotos">
-          <div className="userWallpaper" style={{ width: "100%" }}>
-            <Image src={Foto} style={{ width: "100%", height: "200px" }} />
-          </div>
-          <div className="userInfo d-flex flex-row align-items-center container">
-            <Image src={Foto} style={{ width: "5%" }} roundedCircle />
-            <p className="m-3">Juan perez</p>
-          </div>
-        </div>
-        <div>
-          <p></p>
-          <div className="container">
-            <h4 className="ms-4">Publicaciones</h4>
-            <div></div>
-          </div>
-        </div>
-      </section>
-    </>
-  );
+	const [misAlbums, setMisAlbums] = useState([]);
+	const [dataPhotoUser] = usePhoto();
+	useEffect(() => {
+		cargarDatos();
+		console.log(dataPhotoUser);
+	}, []);
+	const cargarDatos = async () => {
+		try {
+			const result = await getMyAlbumsByIdPhotoUser(dataPhotoUser?.id);
+			console.log('respuesta del backend:', result);
+			if (result?.data?.result) {
+				setMisAlbums([...result.data.result]);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return (
+		<>
+			<section>
+				<div className="userPhotos">
+					<div
+						style={{
+							width: '100%',
+						}}
+					>
+						<Link>
+							<Image src={wallpaper} id="userWallpaper" />
+						</Link>
+					</div>
+					<div className="userInfo d-flex flex-row align-items-center container">
+						<Image src={Foto} roundedCircle className="profileImg" />
+						<p className="profileName">{dataPhotoUser.nombre}</p>
+					</div>
+				</div>
+				<div>
+					<p></p>
+					<div className="container">
+						<h4 className="ms-4">Publicaciones</h4>
+						<div className="d-flex flex-wrap gap-5">
+							{misAlbums.map((item) => (
+								<CardAlbumMain key={myId()} datos={item} />
+							))}
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default PerfilUsuario;
