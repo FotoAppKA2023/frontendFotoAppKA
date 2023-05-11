b5fa004cc3e3bc4d2a3ba7df5a5645e6cc8af0d8
WalterIruegas <walter.iruegas@outlook.com>
2023-05-11 13:52:20 -0500

    Seccion Rollo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,43 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import './Navbar.css';
-import { Navbar, Container, Nav, Button } from 'react-bootstrap';
-<<<<<<< HEAD
-import ModalCrearPublicacion from '../ModalCrearPublicacion/ModalCrearPublicacion.js';
-=======
-import ModalCrearPublicacion from '../ModalCrearPublicacion/ModalCrearPublicacion';
-import { useNavigate } from 'react-router';
-import usePhoto from '../../hooks/usePhoto';
->>>>>>> d59a0e2e5cd0966c5d0d71e88581527793f14c0f
+import React, { useState, useEffect } from "react";
+import "./Navbar.css";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import ModalCrearPublicacion from "../ModalCrearPublicacion/ModalCrearPublicacion.js";
+import SeccionRollos from "../SeccionRollos/SeccionRollos";
+import { useNavigate } from "react-router";
+import usePhoto from "../../hooks/usePhoto";
 
 const AppNavbar = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [showCrearPublicacionModal, setShowCrearPublicacionModal] = useState(false);
+  const [showSeccionRollos, setShowSeccionRollos] = useState(false);
   const navigate = useNavigate();
-  const [dataPhotoUser]=usePhoto();
+  const [dataPhotoUser] = usePhoto();
 
   useEffect(() => {
-    
-    if(dataPhotoUser.isLogged){
-      console.log('Usuario Logeado con exito:..');
+    if (dataPhotoUser.isLogged) {
+      console.log("Usuario Logeado con exito:..");
       setLoggedIn(true);
-    } 
-    
-  }, [dataPhotoUser.isLogged])
-  
+    }
+  }, [dataPhotoUser.isLogged]);
 
   const handleLogin = () => {
     //setLoggedIn(true);
-    navigate('/login');
+    navigate("/login");
   };
 
   const handleCrearPublicacionClick = () => {
     setShowCrearPublicacionModal(true);
   };
+  const handleSeccionRollos = () => {
+    setShowSeccionRollos(true);
+  };
 
   const navLinks = (
     <Nav className="ms-auto">
+      <Nav.Link href="crear-rollo/" onClick={handleSeccionRollos}></Nav.Link>
       <Nav.Link href="#rollos">Rollos</Nav.Link>
-      
+
       <Nav.Link href="crear-rollo/" onClick={handleCrearPublicacionClick}>
         Crear publicación
       </Nav.Link>
@@ -60,7 +59,14 @@ const AppNavbar = () => {
           {loggedIn ? navLinks : loginButton}
         </Container>
       </Navbar>
-      <ModalCrearPublicacion show={showCrearPublicacionModal} onHide={() => setShowCrearPublicacionModal(false)} />
+      <SeccionRollos
+        show={showSeccionRollos}
+        onHide={() => setShowSeccionRollos(false)}
+      />
+      <ModalCrearPublicacion
+        show={showCrearPublicacionModal}
+        onHide={() => setShowCrearPublicacionModal(false)}
+      />
     </>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,21 @@
 
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar.js';
-import RollosPopulares from '../components/RollosPopulares/RollosPopulares.js';
-import ConvierteteExperto from '../components/ConvierteteExperto/ConvierteteExperto';
-import ConoceMas from '../components/ConoceMas/ConoceMas';
-import NuestrosUsuarios from '../components/NuestrosUsuarios/NuestrosUsuarios.js';
-import ExpertoFoto from '../components/ExpertoFoto/ExpertoFoto.js';
-import Footer from '../components/Footer/Footer.js';
-
+//import RollosPopulares from '../components/RollosPopulares/RollosPopulares.js';
+//import ConvierteteExperto from '../components/ConvierteteExperto/ConvierteteExperto';
+//import ConoceMas from '../components/ConoceMas/ConoceMas';
+//import NuestrosUsuarios from '../components/NuestrosUsuarios/NuestrosUsuarios.js';
+//import ExpertoFoto from '../components/ExpertoFoto/ExpertoFoto.js';
+//import Footer from '../components/Footer/Footer.js';
+//import SeccionRollos from '../components/SeccionRollos/SeccionRollos.js';
 const Home = () => {
+	return (
+		<div>
+			<Navbar/>
+		</div>
+	);
+};
+/*const Home = () => {
 	return (
 		<div>
 			<Navbar />
@@ -21,7 +28,7 @@ const Home = () => {
 		</div>
 	);
 };
-
+*/
 
 
 export default Home;
